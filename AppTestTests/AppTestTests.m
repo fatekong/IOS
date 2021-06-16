0939e3ef2d752ec418f6bccc2cb6f6f339100fe2
Pegasus <mingkongc_v7@163.com>
2021-06-16 15:30:51 +0800

    Initial Commit

---
+++ AppTestTests/AppTestTests.m
@@ -0,0 +1,36 @@
+//
+//  AppTestTests.m
+//  AppTestTests
+//
+//  Created by asuswang on 2021/6/16.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AppTestTests : XCTestCase
+
+@end
+
+@implementation AppTestTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
